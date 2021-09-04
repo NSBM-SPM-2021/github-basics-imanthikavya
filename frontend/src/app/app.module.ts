df289d59531e36c913a39ca82f76ca990d599b3f
Kavya Rathnayaka <60612861+imanthikavya@users.noreply.github.com>
2021-09-04 11:43:14 +0530

    #7 create front end for post article

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostarticleComponent } from './postarticle/postarticle.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostarticleComponent,
+
   ],
   imports: [
     BrowserModule,
