7d68ef86e1e7385af20e5223441ca38769c03e4c
Kavya Rathnayaka <60612861+imanthikavya@users.noreply.github.com>
2021-09-06 21:21:04 +0530

    #9 update app-routing.module.ts

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PostarticleComponent } from './postarticle/postarticle.component';
 import { AboutusComponent } from './aboutus/aboutus.component';
+import { ArticlesComponent } from './articles/articles.component';
 
 const routes: Routes = [
   {path:'postarticle',component:PostarticleComponent},
-  {path:'**',redirectTo: '/'},
   {path:'aboutus',component:AboutusComponent},
+  {path:'articles',component:ArticlesComponent},
+  {path:'**',redirectTo: '/'},
 ];
 
 @NgModule({
