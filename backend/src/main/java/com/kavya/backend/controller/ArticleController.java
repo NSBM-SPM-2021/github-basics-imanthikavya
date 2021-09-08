a702b927dec82c78e94547328747656389533e0c
Kavya Rathnayaka <60612861+imanthikavya@users.noreply.github.com>
2021-09-08 09:36:32 +0530

    #6 , #8  update ArticleController.java

--- backend/src/main/java/com/kavya/backend/controller/ArticleController.java
+++ backend/src/main/java/com/kavya/backend/controller/ArticleController.java
@@ -2,18 +2,13 @@ package com.kavya.backend.controller;
 
 import com.kavya.backend.dto.ArticleDto;
 import com.kavya.backend.model.Article;
-import com.kavya.backend.repository.ArticleRepository;
 import com.kavya.backend.service.ArticleService;
-import jdk.swing.interop.SwingInterOpUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 import java.util.List;
-import java.util.Optional;
-import java.util.Scanner;
-import java.util.Set;
 
 @CrossOrigin
 @RestController
